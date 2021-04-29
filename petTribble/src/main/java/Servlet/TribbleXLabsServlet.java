38a820dc2c655ef201af7a6e85a5d53a4b125717
DanielKopeloff <danielkopeloff@gmail.com>
2021-04-29 17:56:25 -0400

    Forgot WEbservlet annotation on tribbleXlabs

--- petTribble/src/main/java/Servlet/TribbleXLabsServlet.java
+++ petTribble/src/main/java/Servlet/TribbleXLabsServlet.java
@@ -5,6 +5,7 @@ import DB.DAO.TribbleXLabDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 
+@WebServlet(urlPatterns = "/tribbleXLabs")
 public class TribbleXLabsServlet extends HttpServlet {
     TribbleXLabDAO tribbleDAO;
 
