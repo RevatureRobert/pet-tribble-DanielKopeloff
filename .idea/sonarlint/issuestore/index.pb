108832f7c95cd33d063c49f9138277cb9e248be3
DanielKopeloff <danielkopeloff@gmail.com>
2021-04-29 17:46:13 -0400

    Got Read All tribbles

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,7 +11,5 @@ l
 <.idea/libraries/Maven__org_postgresql_postgresql_42_2_20.xml,3\7\37926a26d51b26cea28330ce6c9da305e76bc2f6
 [
 +petTribble/src/main/java/Model/Tribble.java,b\b\bb087d9799ca7063b26db60ccbe8da9cd5b27579
-_
-/petTribble/src/main/java/DB/DAO/TribbleDAO.java,8\6\861b9a70cc9c0d998425cf8cde91f53254df4078
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- petTribble/src/main/java/Servlet/TribbleServlet.java
+++ petTribble/src/main/java/Servlet/TribbleServlet.java
@@ -1,11 +1,14 @@
 package Servlet;
 
 
+import DB.DAO.TribbleDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,6 +19,11 @@ public class TribbleServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println("WEll is this working ");
+        TribbleDAO tribbleDAO = TribbleDAO.getInstance();
+        tribbleDAO.readAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(tribbleDAO.readAll());
+
+        writer.println( json);
     }
 }
